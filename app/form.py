43d8882edbfd38f64d2c040fc2c5f431595dff20
itingc <30427164+itingc@users.noreply.github.com>
2019-04-04 12:41:20 -0700

    set up the form

---
+++ app/form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    num1 = StringField('First Integer ', validators=[DataRequired()])
+    num2 = StringField('Second Integer', validators=[DataRequired()])
+    result = SubmitField('Calculate GCF')
+    reset = SubmitField('Clear')
