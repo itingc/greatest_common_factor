7b35761c7f52aa9820f84050a8e90130c8c588d6
itingc <30427164+itingc@users.noreply.github.com>
2019-04-04 22:22:52 -0700

    fix bug
    handling exception where numbers enter aren't positive integer

--- app/form.py
+++ app/form.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class Form(FlaskForm):
-    num1 = StringField('First Integer ', validators=[DataRequired()])
-    num2 = StringField('Second Integer', validators=[DataRequired()])
+    num1 = StringField('First Number', validators=[DataRequired()])
+    num2 = StringField('Second Number', validators=[DataRequired()])
     result = SubmitField('Calculate GCF')
     #reset = SubmitField('Clear')
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,18 @@
 from flask import render_template, flash
 from app import app
 from app.form import Form
-from app.greatest_common_factor import  GreatestCommonFactor
+from app.greatest_common_factor import GreatestCommonFactor
 
 
 @app.route('/', methods=['GET', 'POST'])
 def gcf():
     form = Form()
     if form.validate_on_submit():
-        gcf = GreatestCommonFactor(form.num1.data, form.num2.data)
-        flash(f"GCF = {gcf.calculate_gcf()} ")
-        print(f"GCF = {gcf.calculate_gcf()} ")
+        try:
+            gcf = GreatestCommonFactor(form.num1.data, form.num2.data)
+            flash(f'GCF = {gcf.calculate_gcf()}')
+        except ValueError as e:
+            flash(str(e))
     return render_template('index.html', title='Greatest Common Factor', form=form)
 
 
