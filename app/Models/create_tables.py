d7abe5b9673c7fdae97a6584442353ff49be2c22
itingc <30427164+itingc@users.noreply.github.com>
2019-04-06 10:18:25 -0700

    add database

---
+++ app/Models/create_tables.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+
+conn = sqlite3.connect('gcf.sqlite')
+
+c = conn.cursor()
+c.execute('''
+          CREATE TABLE GCF
+          (x INTEGER NOT NULL,
+           y INTEGER NOT NULL)
+          ''')
+conn.commit()
+conn.close()
