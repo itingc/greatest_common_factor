5e60df82aff1e7ad8c3c51be9695d3fbe1215522
itingc <30427164+itingc@users.noreply.github.com>
2019-04-06 15:31:45 -0700

    Revert "fix bug: add primary key and change database naming"
    This reverts commit 65a860869f222dca7d4c8f097a7f5bc2e2ed60f6.

--- app/Models/create_tables.py
+++ app/Models/create_tables.py
@@ -5,9 +5,8 @@ conn = sqlite3.connect('gcf.sqlite')
 
 c = conn.cursor()
 c.execute('''
-          CREATE TABLE gcf
-          (id INTEGER PRIMARY KEY ASC, 
-           x INTEGER NOT NULL,
+          CREATE TABLE GCF
+          (x INTEGER NOT NULL,
            y INTEGER NOT NULL)
           ''')
 conn.commit()
