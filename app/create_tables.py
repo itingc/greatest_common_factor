606d38534a3d67bf9f7f6efa877abc5d3957ef5a
itingc <30427164+itingc@users.noreply.github.com>
2019-04-06 15:31:15 -0700

    revert

--- app/create_tables.py
+++ app/create_tables.py
@@ -7,9 +7,9 @@ c = conn.cursor()
 c.execute('''
           CREATE TABLE gcf
           (id INTEGER PRIMARY KEY ASC, 
-           x INTEGER NOT NULL ,
-           y INTEGER NOT NULL,
-           gcf INTEGER NOT NULL)
+           x INTEGER ,
+           y INTEGER ,
+           gcf INTEGER )
           ''')
 conn.commit()
 conn.close()
