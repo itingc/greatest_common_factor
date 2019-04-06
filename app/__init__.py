5bc820df94c4ec5a639809ab0a4acea283abf3a6
itingc <30427164+itingc@users.noreply.github.com>
2019-04-05 21:39:57 -0700

    fix bug: adding custom style sheet

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+import os
 
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'dafawefawefeahtrdhdr'
 app.config.from_object(Config)
+
+app._static_folder = os.path.join(os.getcwd(), 'static')
 
 
 from app import routes
