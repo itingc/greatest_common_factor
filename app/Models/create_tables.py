65a860869f222dca7d4c8f097a7f5bc2e2ed60f6
itingc <30427164+itingc@users.noreply.github.com>
2019-04-06 11:00:22 -0700

    fix bug: add primary key and change database naming

--- app/Models/create_tables.py
+++ app/Models/create_tables.py
@@ -5,8 +5,9 @@ conn = sqlite3.connect('gcf.sqlite')
 
 c = conn.cursor()
 c.execute('''
-          CREATE TABLE GCF
-          (x INTEGER NOT NULL,
+          CREATE TABLE gcf
+          (id INTEGER PRIMARY KEY ASC, 
+           x INTEGER NOT NULL,
            y INTEGER NOT NULL)
           ''')
 conn.commit()
