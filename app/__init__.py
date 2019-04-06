abf738f39d4f3ef55e21bbe88959387bd2d64642
itingc <30427164+itingc@users.noreply.github.com>
2019-04-06 00:21:11 -0700

    wrap up

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,11 @@ import os
 
 app = Flask(__name__)
 
+# Add simple security to prevent attack
 app.config['SECRET_KEY'] = 'dafawefawefeahtrdhdr'
 app.config.from_object(Config)
 
+# Set directory for static folder
 app._static_folder = os.path.join(os.getcwd(), 'static')
 
 
