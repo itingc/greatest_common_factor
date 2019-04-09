a01f255a02ca68d116d6c5e0396ac0366b882de2
itingc <30427164+itingc@users.noreply.github.com>
2019-04-09 14:24:58 -0700

    clean up (looking for ways to retrive data from '/gcf_result')

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
-from flask import render_template, flash, request
+from flask import render_template, request
 from app import app
-from app.form import Form
 from app.greatest_common_factor import GreatestCommonFactor
 import json
 
@@ -8,32 +7,24 @@ import json
 @app.route('/')
 def gcf():
     return render_template('index.html')
-# def gcf():
-#     form = Form()
-#     if form.validate_on_submit():
-#         try:
-#             gcf = GreatestCommonFactor(form.num1.data, form.num2.data)
-#             flash(f'GCF = {gcf.calculate_gcf()}')
-#             response = app.response_class(
-#                 status=300,
-#                 response=json.dumps({'First Number': form.num1.data, 'Second Number': form.num2.data}),
-#                 mimetype='application/json'
-#             )
-#             return response
-#         except ValueError as e:
-#             flash(str(e))
-#     return render_template('index.html', title='Greatest Common Factor', form=form)
 
 
 @app.route('/gcf_result', methods=['POST'])
 def gcf_result():
     num1 = request.form['num1']
     num2 = request.form['num2']
-    gcf = GreatestCommonFactor(num1, num2)
-    result = gcf.calculate_gcf()
-    response = app.response_class(
-                    status=200,
-                    response=json.dumps({'First Number': num1, 'Second Number': num2, 'GCF': result}),
-                    mimetype='application/json'
-                )
+    try:
+        gcf = GreatestCommonFactor(num1, num2)
+        result = gcf.calculate_gcf()
+        response = app.response_class(
+                        status=200,
+                        response=json.dumps({'First Number': num1, 'Second Number': num2, 'GCF': result}),
+                        mimetype='application/json'
+        )
+    except ValueError as e:
+        response = app.response_class(
+            status=400,
+            response=str(e)
+        )
+
     return response
